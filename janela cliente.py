bd019e6ae0d232b6ddebd03afcdeb7500b05be21
vinicius-deassis <vassisprof@gmail.com>
2022-10-01 17:21:29 -0300

    terceira versao

--- janela cliente.py
+++ janela cliente.py
@@ -10,11 +10,11 @@ stock1 = json.load(file)
 file.close()
 
 layout = [
-    [sg.Text("Bem vindo -usuario-!")],
+    [sg.Text(f'Bem vindo -usuario-!')],
     [sg.Text("Escolha um produto.")],
-    [sg.Combo(values=[stock["1"][0], stock["2"][0], stock["3"][0], stock["4"][0], stock["5"][0]], key='product')],
+    [sg.Combo(values=[stock["1"][0], stock["2"][0], stock["3"][0], stock["4"][0], stock["5"][0]], key='product', size=(27, 1))],
     [sg.Button('Ver disponibilidade')],
-    [sg.Text("Qual a quantidade desejada?"), sg.Input(key='amount', size=(6, 1))],
+    [sg.Text("Qual a quantidade desejada?"), sg.Input(key='amount', size=(3, 1))],
     [sg.Button('Adicionar ao carrinho')]
 
 ]
@@ -28,8 +28,24 @@ while True:
         product_name = values['product']
         sg.Popup(product_name, f'Quantidade no estoque: {stock1[product_name][0]}', f'Preço: R${stock1[product_name][1]}')
     if event == 'Adicionar ao carrinho':
-        cart = []
-        user = (values['product'], values['amount'])
-        cart.append(user)
-        print(cart)
+        list_stock = stock1[values['product']]
+        amount_in_stock = list_stock[0]
+        request = values['amount']
+        if int(request) > int(amount_in_stock):
+            sg.Popup('Nao temos essa quantidade no estoque')
+        else:
+            sg.Popup('Items adicionados ao carrinho!')
+            eq = int(amount_in_stock) - int(request)
+            previous_amount = stock1[values['product']][0]
+            print(previous_amount)
+            stock1[values['product']][0] = eq
+            print(stock1[values['product']])
+
+            #cart = {}
+            #user_cart = (values['product'], values['amount'])
+            #cart.update({1})
+            #file = open('cart.json', 'w')
+
+
+
 
